e78a6f4df9f950bb757f3dc6bc125fbde46f2c6c
briangor <briangor13@gmail.com>
2022-08-31 19:48:54 +0300

    update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#/">VueJS</a>
+      <a class="navbar-brand" href="#/">
+        <img src="../assets/images/logo.png" alt="" class="nav-logo w-1 h-1">
+        VueJS Students
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,7 +12,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/">Home</a>
+            <a class="nav-link" href="#/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#/students">Students</a>
@@ -17,14 +20,11 @@
           <li class="nav-item">
             <a class="nav-link" href="#/courses">Courses</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#/about">About</a>
-          </li>
         </ul>
-        <form class="d-flex" role="search">
+        <div class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        </div>
       </div>
     </div>
   </nav>
