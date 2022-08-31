7065aced187b9ac78dfe586879abac5423028873
briangor <briangor13@gmail.com>
2022-08-31 23:45:23 +0300

    updated StudentGrades.vue

--- src/components/students/StudentGrades.vue
+++ src/components/students/StudentGrades.vue
@@ -2,6 +2,7 @@
     <div class="student-grades">
       <h5>Student Grades Component</h5>
       <p>{{ studentSchema }}</p>
+      <input type="text" placeholder="Seen by">
     </div>
 </template>
 <script>
