0a57c3b0bfb7f21919ab619e7880677c24e25c36
briangor <briangor13@gmail.com>
2022-08-31 18:40:35 +0300

    add Async.vue

---
+++ src/components/global/Async.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h5>Async Component</h5>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AsyncComponent',
+    methods: {
+        getMessage(resolve, reject) {
+            setTimeout(function () {
+                // Pass the component definition to the resolve callback
+                resolve({
+                    template: '<div>I am async!</div>'
+                })
+            }, 1000)
+        }
+    }
+}
+</script>
+<style>
+</style>
