ed6e242456df5617a37df5b90ecb5864a25be6e7
briangor <briangor13@gmail.com>
2022-08-31 18:24:21 +0300

    add StudentGrades.vue

---
+++ src/components/students/StudentGrades.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="student-grades">
+      <h5>Student Grades Component</h5>
+      <p>{{ studentSchema }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'StudentGrades',
+    inject: ['studentSchema']
+}
+</script>
+<style>
+  .student-grades {
+    padding: 2rem 1rem;
+  }
+</style>
