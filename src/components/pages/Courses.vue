819e04ebf5dd25648e1a08ad05e11462e4e5ce17
briangor <briangor13@gmail.com>
2022-08-31 19:48:06 +0300

    add Courses.vue

---
+++ src/components/pages/Courses.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="courses">
+        <h5>Courses offered</h5>
+        <div v-for="course in courses" :key="course.id">
+            <li>{{ course.title }}</li>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CoursesPage',
+    data() {
+        return{ 
+            courses:[
+                {
+                    id: 1,
+                    title: 'Vue Instance'
+                },
+                {
+                    id: 2,
+                    title: 'Vue Router'
+                },
+                {
+                    id: 3,
+                    title: 'Lifecycle hooks in Vue'
+                }
+            ]
+        }
+    }
+    
+}
+</script>
+<style></style>
