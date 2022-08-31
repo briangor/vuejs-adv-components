cf56631ff3a8bbe046667a528499d1bf84a66723
briangor <briangor13@gmail.com>
2022-08-31 12:45:55 +0300

    setup component organization

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+const NotFound = { template: '<p>Page not found</p>' }
+const Home = { template: '<p>home page</p>' }
+const About = { template: '<p>about page</p>' }
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
+
 new Vue({
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
   render: h => h(App),
 }).$mount('#app')
