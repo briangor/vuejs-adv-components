184c05f4f0f5d6a3dff6029f4da4d41e4c95def2
briangor <briangor13@gmail.com>
2022-09-01 12:42:51 +0300

    test async component in main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap'; 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/style.scss'; 
 //import DefaultButton from "./components/global/DefaultButton.vue";
 
 Vue.config.productionTip = false;
 //Vue.component('DefaultButton', DefaultButton)
+
+// Vue.component('async-example', function (resolve) {
+//   setTimeout(function () {
+//     // Pass the component definition to the resolve callback
+//     //resolve( import('./components/') )
+
+//   }, 1000)
+// })
+
+
 new Vue({
   render: h => h(App),
   data: {
@@ -22,12 +33,3 @@ new Vue({
     }
   }
 }).$mount('#app')
-
-Vue.component('async-example', function (resolve) {
-  setTimeout(function () {
-    // Pass the component definition to the resolve callback
-    resolve({
-      template: '<div>I am async!</div>'
-    })
-  }, 1000)
-})
