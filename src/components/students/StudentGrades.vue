1907ae7535d6dd037127dca30c25197b463339c7
briangor <briangor13@gmail.com>
2022-09-01 08:55:42 +0300

    update student grades layout

--- src/components/students/StudentGrades.vue
+++ src/components/students/StudentGrades.vue
@@ -1,14 +1,31 @@
 <template>
     <div class="student-grades">
-      <h5>Student Grades Component</h5>
-      <p>{{ studentSchema }}</p>
-      <input type="text" placeholder="Seen by">
+      <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-default">Basics</span>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-default">Routing</span>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-default">Data Handling</span>
+      <input type="text" class="form-control" >
+    </div>
+      
+      <input type="text" placeholder="Signed by">
     </div>
 </template>
 <script>
 export default {
     name: 'StudentGrades',
-    inject: ['studentSchema']
+    inject: ['studentSchema'],
+    methods: {
+      getStudentSchema(){
+        alert(studentSchema)
+        console.log(studentSchema);
+      }
+    }
 }
 </script>
 <style>
