1bf7d0211d3ff8be16065085f3a2ffe90e0eaa30
briangor <briangor13@gmail.com>
2022-09-01 08:54:44 +0300

    update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid">
       <a class="navbar-brand" href="#/">
         <img src="../assets/images/logo.png" alt="" class="nav-logo w-1 h-1">
-        VueJS Students
+        VueJS School
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
