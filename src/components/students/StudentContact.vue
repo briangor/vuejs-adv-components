41c3b48ee28ca6f161abd735674a9730d182da64
briangor <briangor13@gmail.com>
2022-09-01 08:55:30 +0300

    update student contact layout

--- src/components/students/StudentContact.vue
+++ src/components/students/StudentContact.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>
-      <p>Student Contact</p>
-      <input type="text" placeholder="">
+  <div class="std-contact">
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-default">First Name</span>
+      <input type="text" class="form-control" >
     </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-default">Last Name</span>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-default">Email</span>
+      <input type="text" class="form-control" >
+    </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'StudentContact'
+  name: 'StudentContact'
 }
 </script>
+<style>
+  .std-contact {
+    margin-top: 1rem;
+    padding: 1rem;
+  }
+</style>
