b524dec3b517860f1b9a04231f404a4c93999739
briangor <briangor13@gmail.com>
2022-09-01 08:54:59 +0300

    add courses data

--- src/components/pages/Courses.vue
+++ src/components/pages/Courses.vue
@@ -1,34 +1,62 @@
 <template>
     <div class="courses">
-        <h5>Courses offered</h5>
-        <div v-for="course in courses" :key="course.id">
+        <h5>Courses Offered</h5>
+        <div v-for="course in cData" :key="course.id">
             <li>{{ course.title }}</li>
         </div>
+
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'CoursesPage',
-    data() {
-        return{ 
-            courses:[
-                {
-                    id: 1,
-                    title: 'Vue Instance'
-                },
-                {
-                    id: 2,
-                    title: 'Vue Router'
-                },
-                {
-                    id: 3,
-                    title: 'Lifecycle hooks in Vue'
-                }
-            ]
-        }
+import Footer from '../global/Footer.vue';
+let courses = [
+    {
+        "id": 1,
+        "title": "VueJS Basics"
+    },
+    {
+        "id": 2,
+        "title": "Vue Routing"
+    },
+    {
+        "id": 3,
+        "title": "Handling Asynchronous Data"
+    },
+    {
+        "id": 4,
+        "title": "Axios and Its Implementation"
+    },
+    {
+        "id": 5,
+        "title": "VueJS Dynamic Components"
+    },
+    {
+        "id": 6,
+        "title": "Vue and The Ecosystem"
+    },
+    {
+        "id": 7,
+        "title": "Vue Instance"
+    },
+    {
+        "id": 8,
+        "title": "Lifecycle hooks in Vue"
     }
-    
+]
+export default {
+    name: "CoursesPage",
+    data() {
+        return {
+           cData: courses
+        }
+    },
+    components: { Footer }
 }
 </script>
-<style></style>
+<style>
+    .courses {
+        padding: 2rem 4rem;
+    }
+</style>
