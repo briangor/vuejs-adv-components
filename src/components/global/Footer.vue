e876b7d6298e99ead46883d8bcdc11e3574ccf64
briangor <briangor13@gmail.com>
2022-09-01 08:21:12 +0300

    add Footer.vue

---
+++ src/components/global/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="footer">
+        <p>Legal Stuff | Privacy Policy</p>
+        <p>© VueSchool 2022</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'FooterSection'
+}
+</script>
+<style>
+.footer {
+    border-top: .5px solid rgb(230, 230, 230);
+    margin-top: 3rem;
+    text-align: center;
+    padding: 2rem 0;
+    font-size: .7rem;
+    color: rgb(61, 61, 61);
+}
+</style>
